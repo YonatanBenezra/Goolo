6bdad833dc891f27d7286a8b28ae1676b61398fa
Hussam Khan <hussamkhan98@gmail.com>
2021-01-28 08:26:35 +0500

    fixed user login state and disappering of products after a page refresh

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React from 'react'
 import { observer, inject } from 'mobx-react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import {
+	BrowserRouter as Router,
+	Route,
+	Switch,
+	Redirect,
+} from 'react-router-dom'
 import Home from './components/landing/Home'
 import Swiper from './components/swiper/Swiper'
 import BagRecommendations from './components/bag/Recommendations/BagRecommendations'
 import SimpleBottomNavigation from './components/Navbar/Navbar'
-const App = inject('UserStore')(observer(props => {
-  return (
-      <Router>
-        <Switch>
-          <Route exact path={'/'} render={props => <Home {...props}/> } />
-          <Route exact path={'/bagRecommendations'} render={props => <BagRecommendations /> } />
-          <Route exact path={'/swiper'} render={props => <Swiper/> } />
-        </Switch>
-      {props.UserStore.isLoggedIn && <SimpleBottomNavigation />}
-      </Router>
-  )
-}))
+const App = inject('UserStore')(
+	observer(props => {
+		return (
+			<Router>
+				<Switch>
+					<Route exact path={'/'} render={props => <Home {...props} />} />
+					<Route
+						exact
+						path={'/bagRecommendations'}
+						render={props => <BagRecommendations />}
+					/>
+					<Route exact path={'/swiper'} render={props => <Swiper />} />
+					<Redirect to="/" from="*" />
+				</Switch>
+				{props.UserStore.isLoggedIn && <SimpleBottomNavigation />}
+			</Router>
+		)
+	}),
+)
 
 export default App
--- src/components/landing/Home.jsx
+++ src/components/landing/Home.jsx
@@ -1,34 +1,47 @@
-import React, { useState } from "react";
-import "./style.css";
+import React, { useState, useEffect } from 'react'
+import './style.css'
 import Login from './Login'
 import Register from './SignUp'
 import { observer, inject } from 'mobx-react'
 
-const Home = inject('UserStore')(observer(props => {
-  const [isLogginActive, setIsLogginActive] = useState(true)
+const Home = inject('UserStore')(
+	observer(props => {
+		const [isLogginActive, setIsLogginActive] = useState(true)
 
-  const redirectUser = () => {
-    function swiper () {
-      props.history.push('/swiper')
-    }
+		useEffect(() => {
+			if (localStorage.getItem('user')) {
+				props.UserStore.setUser(JSON.parse(localStorage.getItem('user')))
+				redirectUser()
+			}
+		}, [])
 
-    setTimeout(swiper, 1000)
-  }
+		const redirectUser = () => {
+			function swiper() {
+				props.history.push('/swiper')
+			}
+			setTimeout(swiper, 1000)
+		}
 
-  return (
-    <div className='homeContainer'>
-      {isLogginActive ? 
-      <>
-        <Login redirectUser={redirectUser} />
-        <div className="text" onClick={() => setIsLogginActive(false) }>Click here to Register</div>
-      </> :
-      <>
-        <Register redirectUser={redirectUser} />
-        <div className="text" onClick={() => setIsLogginActive(true) }>Already have an account ? Log-In</div>
-      </>}
-
-    </div>
-  )
-}))
+		return (
+			<div className="homeContainer">
+				{isLogginActive ? (
+					<>
+						<Login redirectUser={redirectUser} />
+						<div className="text" onClick={() => setIsLogginActive(false)}>
+							Click here to Register
+						</div>
+					</>
+				) : (
+					<>
+						<Register redirectUser={redirectUser} />
+						<div className="text" onClick={() => setIsLogginActive(true)}>
+							Already have an account ? Log-In
+						</div>
+					</>
+				)}
+			</div>
+		)
+	}),
+)
 
 export default Home
